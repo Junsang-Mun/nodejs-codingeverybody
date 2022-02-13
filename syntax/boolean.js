53d86ed99c1e89f110c3b634972118464b0644e4
Junsang-mun <mun.js@aol.com>
2022-02-13 13:49:02 +0900

    syntax - Boolean & Equal operator

---
+++ syntax/boolean.js
@@ -0,0 +1,13 @@
+console.log(true);
+console.log(false);
+console.log("\n\n");
+// equal operator 과 strict equal operator 비교
+
+let one_number = 1;
+let one_string = '1';
+
+console.log("one_number == one_string:  ");
+console.log(one_number == one_string + "\n");
+
+console.log("one_number === one_string:  ");
+console.log(one_number === one_string);
