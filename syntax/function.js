490319b72618f2953cd9e9b7ebbb9a13dbc52707
Junsang-mun <mun.js@aol.com>
2022-02-17 10:27:27 +0900

    function 인자 받아오기

--- syntax/function.js
+++ syntax/function.js
@@ -1,9 +1,18 @@
-f123();
+f123(true);
 console.log('A');
 console.log('B');
 f123();
+f123(false);
 
-function f123(){
+
+function f123(goornot){
     console.log(1);
     console.log(2);
+    if(goornot === true){
+        console.log('Hell yeah!');
+    } else if(goornot === false){
+        console.log('Heaven yell...');
+    } else{
+        console.log('Null Null')
+    }
 }
