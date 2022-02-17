cc645e452ef6ac029c9bca0028c7e77822d46b67
Junsang-mun <mun.js@aol.com>
2022-02-17 11:32:09 +0900

    main.js 리팩토링 with 함수

--- main.js
+++ main.js
@@ -2,67 +2,60 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-let title;
-let description;
-let list = '';
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist){
+  let list = '<ul>';
+  let i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + '</ul>';
+  return list;
+}
 
 let app = http.createServer(function(request,response){
     let _url = request.url;
     let querydata = url.parse(_url, true).query;
     let pathname = url.parse(_url, true).pathname;
-    title = querydata.id
     
     if(pathname === '/') {
       if(querydata.id === undefined){
-        title = 'Hello';
-        description = 'hello there!';
-
-        console.log(title);
-        fs.readdir('data/', 'utf8', function(err, filelist){
-        console.log(filelist);
-
-        let i = 0;
-        while(i < filelist.length){
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i++;
-          console.log(i);
-        } //TODO: 무한생성 이슈 fix
-
-        /*
-        let list = '';
-        let i = 0;
-        while(i < filelist.length){
-          list = list + `<li><a href="${filelist[i]}.html">${filelist[i]}</a></li>`
-        }
-        메모리 누수 의심 위치
-        */
-        let template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="index.html">WEB</a></h1>
-          <ol>
-            ${list}
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}
-          </p>
-          </body>
-          </html>
-          `;
+        fs.readdir('./data', function(error, filelist){
+          const title = 'Welcome';
+          const description = 'Hello Node.js';
+          const list = templateList(filelist);
+          const template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-      });
+        });
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${querydata.id}`, 'utf8', function(error, description){
+            const title = querydata.id;
+            const list = templateList(filelist);
+            const template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
       }
-      
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
     }
- 
-});
+  });
 app.listen(3000);
